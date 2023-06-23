7db6eb6db7221a5aa3aadbd44bb0c07ce7b1a6a3
Beengoo <105955419+Beengoo@users.noreply.github.com>
2023-06-23 22:53:20 +0300

    utils.py bug fix
    i forgot to update this file

--- utils.py
+++ utils.py
@@ -1,12 +1,31 @@
 import json
 import aiofiles
-from discord import Interaction
+from discord import Interaction, Member
 
 
 async def read_json(file: str):
     async with aiofiles.open(file, "r", encoding='utf-8') as f:
         content = await f.read()
         return json.loads(content)
+
+
+async def check_blocked_role(author: Member):
+    data = synchron_read_json("assets/configs/rank.json")["roleIdsBlackList"]
+    user_roles = []
+    for roleObj in author.roles:
+        user_roles.append(roleObj.id)
+    for id_ in data:
+        if id_ in user_roles:
+            return True
+    return False
+
+
+async def check_blocked_channel(channel):
+    data = synchron_read_json("assets/configs/rank.json")["roleIdsBlackList"]
+    for id_ in data:
+        if channel.id == id_:
+            return True
+    return False
 
 
 def synchron_read_json(file: str):
